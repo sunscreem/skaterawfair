d92f3dc3157fc31afba45605659f3b96cafd8ce0
Rob Cooper <sunscreem@gmail.com>
2017-04-26 20:38:04 +0100

    Added holding page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('content')
+   
+    <section id="ceremony">
+        <div class="line"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Oh no - 404</h1>
+                    <h1>Page Not Found!</h1>
+                    <p class="text-center"><a class="btn btn-lg btn-primary">Return To Home Page</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@
 */
 
 Route::get('/', function () {
+    return view('holding');
+});
+
+Route::get('/new', function () {
     return view('welcome');
 });
 
